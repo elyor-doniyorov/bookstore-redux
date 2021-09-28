98888d9484fe67974451cd9d8fa9fc80279b6438
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-28 17:49:42 +0500

    Install bootsrap, fontawesome

--- src/components/appComponents/Books.js
+++ src/components/appComponents/Books.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BookList from './booksComponents/BookList';
-import AddBookForm from './booksComponents/AddBookForm';
+import AddBookForm from './booksComponents/AddBookForm'; // import/extensions
 
 function Books() {
   return (
