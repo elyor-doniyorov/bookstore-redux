16790293f5342b162400a2fd2955b37d4f6579dd
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-30 11:10:46 +0500

    add changes to form for adding books and booklist

--- src/components/appComponents/booksComponents/AddBookForm.js
+++ src/components/appComponents/booksComponents/AddBookForm.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../../redux/books/books';
 
 function AddBookForm() {
+  const dispatch = useDispatch();
+  const [formState, setFormState] = useState({ title: '', author: '', category: 'Category' });
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    dispatch(addBook(formState));
+    e.preventDefault();
+    setFormState({
+      ...formState, title: '', author: '', category: 'Category',
+    });
+  }
+
   return (
     <div>
       <h2>Add NEW BOOK</h2>
-      <form>
-        <input type="text" name="title" placeholder="Book title" />
-        <input type="text" placeholder="Author" />
-        <select name="category" defaultValue="Category">
-          <option value="">Category</option>
-          <option value="">Sci-Fi</option>
-          <option value="">Action</option>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" placeholder="Book title" value={formState.title} onChange={handleChange} />
+        <input type="text" name="author" placeholder="Author name" value={formState.author} onChange={handleChange} />
+        <select name="category" value={formState.category} onChange={handleChange}>
+          <option value="" hidden>Category</option>
+          <option value="Sci-Fi">Sci-Fi</option>
+          <option value="Action">Action</option>
+          <option value="Action">Economy</option>
         </select>
-        <button type="button" className="add-btn btn-primary">
-          ADD BOOK
-        </button>
+        <button type="submit" className="add-btn">ADD BOOK</button>
       </form>
     </div>
   );
--- src/components/appComponents/booksComponents/BookList.js
+++ src/components/appComponents/booksComponents/BookList.js
@@ -6,20 +6,21 @@ function BookList() {
   const books = useSelector((state) => state.books);
   return (
     <div className="book-list">
-      {books.map((book) => {
+      { books.map((book) => {
         const {
           id, title, author, category, progress,
         } = book;
         return (
           <BookItem
             key={id}
+            id={id}
             title={title}
             author={author}
             cat={category}
             prog={progress}
           />
         );
-      })}
+      }) }
     </div>
   );
 }
