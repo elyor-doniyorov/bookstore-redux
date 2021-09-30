07527034cd48a7ffbed186600581364e02401582
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-30 11:02:53 +0500

    Install redux-logger

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,27 +1,35 @@
-const defaultState = [
-  {
-    id: 1,
-    title: 'The Hunger Games',
-    author: 'Suzanne collins',
-    category: 'Action',
-    progress: 54,
-  },
-  {
-    id: 2,
-    title: 'Dune',
-    author: 'Frank herbert',
-    category: 'Sci-Fi',
-    progress: 64,
-  },
-  {
-    id: 3,
-    title: 'capital In the twenty-First Century',
-    author: 'Suzanne collins',
-    category: 'Economy',
-    progress: 78,
-  },
-];
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-export default function books(state = defaultState) {
-  return state;
+const defaultState = [];
+
+export default function books(state = defaultState, action) {
+  switch (action.type) {
+    case ADD_BOOK:
+      return state.concat({
+        id: Date.now(),
+        title: action.payload.title,
+        author: action.payload.author,
+        category: action.payload.category,
+        progress: 0,
+      });
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+}
+
+export function addBook(payload) {
+  return {
+    type: ADD_BOOK,
+    payload,
+  };
+}
+
+export function removeBook(payload) {
+  return {
+    type: REMOVE_BOOK,
+    payload,
+  };
 }
