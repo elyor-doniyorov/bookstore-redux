3747b3afe53d4d612cd55edd159e1951d7f52d3b
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-10-01 10:03:50 +0500

    Add styles to book items

--- src/components/appComponents/booksComponents/bookListComponents/BookItem.js
+++ src/components/appComponents/booksComponents/bookListComponents/BookItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBookAPI } from '../../../../redux/books/books';
+import './BookItem.css';
 
 function BookItem(props) {
   const {
@@ -15,32 +16,54 @@ function BookItem(props) {
     dispatch(removeBookAPI(payload));
   }
 
+  const progressStyle = {
+    position: 'relative',
+    backgroundImage: `conic-gradient(from 0deg, #307bbe, #379cf6 ${prog}%, #e8e8e8 ${prog}%, #e8e8e8)`,
+    display: 'inline-block',
+    width: '4.25rem',
+    height: '4.25rem',
+    borderRadius: '50%',
+  };
+
+  const innerWhite = {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    width: '3.5rem',
+    height: '3.5rem',
+    borderRadius: '50%',
+    top: '50%',
+    left: '50%',
+    transform: 'translate( -50%, -50%)',
+  };
+
   return (
     <div className="book-item">
-      <div className="col">
+      <div className="col book-details-col">
         <div className="category-name">{cat}</div>
         <div className="book-title">{title}</div>
-        <div className="author-name">{author}</div>
-        <div>
+        <div className="book-author">{author}</div>
+        <div className="book-btns">
           <button type="button">Comments</button>
           <button type="button" onClick={handleClick} data-bookid={id}>Remove</button>
           <button type="button">Edit</button>
         </div>
       </div>
-      <div className="col">
-        <span className="progress-chart" />
+      <div className="row">
+        <span className="progress-chart" style={progressStyle} />
+        <div style={innerWhite} />
         <span>
-          {prog}
-          %
-          <br />
-          {' '}
-          completed
+          <div className="progress-percentage">
+            {prog}
+            %
+          </div>
+          <div className="completed">completed</div>
         </span>
       </div>
-      <div className="col">
-        <span>CURRENT CHAPTER</span>
-        <span>CHAPTER 17</span>
-        <button type="button">UPDATE PROGRESS</button>
+      <div className="vline" />
+      <div className="col update-col">
+        <span className="current-chapter">CURRENT CHAPTER</span>
+        <span className="current-lesson">CHAPTER 17</span>
+        <button type="button" className="update-progress-btn">UPDATE PROGRESS</button>
       </div>
     </div>
   );
